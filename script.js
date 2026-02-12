651f1b7eaad9f1f3bc9bc3a204a3801d48f2c6ee
eduardo-4 <78921641+eduardo-4@users.noreply.github.com>
2026-02-12 13:39:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+   "photo0.jpg", // The starting photo
+    "photo1.jpg", // First "No"
+    "photo2.jpg", // Second "No"
+    "photo3.jpg", // Third "No"
+    "photo4.jpg", // Fourth "No"
+    "photo5.jpg", // Fifth "No"
+    "photo6.jpg", // Sixth "No"
+    "photo7.jpg", // Sixth "No"
+    "photo8.jpg"  // Last "No"
 ]
 
 const noMessages = [
     "No",
-    "Are you positive? 🤔",
-    "Pookie please... 🥺",
-    "If you say no, I will be really sad...",
-    "I will be very sad... 😢",
-    "Please??? 💔",
+    "Ellieeee, click yes!",
+    "What are you doing Ellie...",
+    "You are hurting me...",
+    "Bruh",
+    "That's mean",
     "Don't do this to me...",
-    "Last chance! 😭",
+    "CLick yes already!",
     "You can't catch me anyway 😜"
 ]
 
